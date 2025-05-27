fcd3f9b9c9efe4bc8d0e6def5e42f1c4c40570b1
Maxim Bogdanov <126229962+dtfyu3@users.noreply.github.com>
2025-05-27 09:14:22 +0300

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const config = {
+    url: process.env.GOOGLE_URL,
+    key: process.env.API_KEY,
+};
+
+
+
+// Обработчик вебхука
+module.exports = async (req, res) => {
+    if (req.method !== 'POST') return res.status(405).end();
+
+    try {
+        const body = req.body;
+        const response = await axios.post(config.url, body);
+        console.log(response.data);
+        res.status(200).json({ status: "OK" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+};
